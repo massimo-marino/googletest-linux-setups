c36f2c3b9d8f9c23d5186bb059221b2860f8e59e
Massimo Marino <massimo.marino@ieee.org>
2020-12-31 20:58:42 +0100

    standard installation: cmake can find gmock libs: added FindGMock.cmake script and updated the example

--- standard/example/main.cpp
+++ standard/example/main.cpp
@@ -9,6 +9,7 @@ TEST(test_case, example_test)
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
+//    testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
---
+++ standard/install-findgmock-cmake.sh
@@ -0,0 +1,4 @@
+# The file FindGMock.cmake can be retrieved from:
+#  https://www.projectiwear.org/home/svn/iwear/src/trunk/vim/_vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/cmake/FindGMock.cmake
+#
+sudo cp -v ./FindGMock.cmake /usr/share/cmake-3.5/Modules/
